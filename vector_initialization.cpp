33cd65edd9f161827bdbb5634d6f6cb15716b709
tanvirrifat1 <rifatkhan5567790@gmail.com>
2026-07-01 20:50:19 +0600

    added

--- vector_initialization.cpp
+++ vector_initialization.cpp
@@ -1,13 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
 
-    vector<int> v(4, 2);
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
-    }
+    vector<int> v = {2, 8, 7, 9, 2, 3};
+
+    // replace(v.begin(), v.end() - 1, 2, 100);
+
+    // for (int x : v)
+    // {
+    //     cout << x << " ";
+    // }
+
+    cout << v[2] << endl;
+
+    cout << v.back() << endl;
+
     return 0;
 }
--- vector_input.cpp
+++ vector_input.cpp
@@ -6,17 +6,21 @@ using namespace std;
 
 int main()
 {
+
     int n;
+
     cin >> n;
 
-    int sum = 0;
+    vector<int> v(n);
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        sum += i;
+        cin >> v[i];
     }
-
-    cout << sum;
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
 
     return 0;
 }
