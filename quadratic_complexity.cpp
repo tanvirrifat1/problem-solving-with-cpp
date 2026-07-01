f0a36f8aeeae9d0a69ab8060cc38fa83ca39b092
tanvirrifat1 <rifatkhan5567790@gmail.com>
2026-07-01 19:43:59 +0600

    added

--- quadratic_complexity.cpp
+++ quadratic_complexity.cpp
@@ -14,7 +14,7 @@ int main()
     for (int i = 1; i <= n; i++)
     {
 
-            for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= n; j++)
         {
             cout << "hello" << endl;
         }
